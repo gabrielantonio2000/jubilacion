fef4589f14d09e0a93e7fbd0422ddbc201f8ca20
gabrielantonio2000 <gabrielantonio21oc@gmail.com>
2024-08-21 19:26:48 -0600

    optimizando las pruebas usando test con configurado en el package.json

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: "node",
-  testPathIgnorePatterns: [
-    "<rootDir>/anaconda3/",
-    "<rootDir>/.vscode/",
-    "<rootDir>/importantes proyectos seminario/",
-    "<rootDir>/Desktop/barberia/",
-  ],
-};
